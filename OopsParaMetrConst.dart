029c1d47a67507bec988ce67bd8359ac687fd5d9
Murtazakatyar <168894683+Murtazakatyar@users.noreply.github.com>
2024-06-05 11:20:19 +0500

    Add files via upload
    dart oops parameter constructor

---
+++ OopsParaMetrConst.dart
@@ -0,0 +1,13 @@
+void main() {
+  var obj = ParaConst('Arslan', 35500);
+}
+       
+class ParaConst {
+
+ //this is parameter constructor
+ 
+  ParaConst(String name, int sallary) {
+    print('Name  : $name');
+    print('Sallary : $sallary');
+  }
+}
